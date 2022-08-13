dba1d23693631415815393cb6e918cc51ef9c0cf
Eliezer Azevedo <eliezer_azevedo@hotmail.com>
2022-08-13 17:52:48 -0300

    Lab de padroes de projeto JAVA

---
+++ src/one/digitalinnovation/gof/singleton/SingletonEager.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.gof.singleton;
+
+
+/**
+ * Singleton "apressado".
+ * @author eliez
+ */
+public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+
+    }
+
+    public static SingletonEager getInstancia() {
+
+        return instancia;
+
+    }
+}
